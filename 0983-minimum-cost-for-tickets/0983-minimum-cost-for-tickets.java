db1440505daebb261684eb073208a177395011bd
Aviraj-create <81038098+Aviraj-create@users.noreply.github.com>
2023-04-02 14:51:04 +0530

    Time: 2 ms (48.18%), Space: 41.2 MB (33.69%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -20,9 +20,33 @@ class Solution {
         return dp[index]=Math.min(option1,Math.min(option2,option3));
         
     }
+    
+     public int solveTab(int[] days, int[] costs)
+    {
+         int n=days.length;
+        int[]dp=new int[n+1];
+         Arrays.fill(dp,Integer.MAX_VALUE);
+         dp[n]=0;
+         
+         for(int k=n-1;k>=0;k--)
+         {
+             int option1=costs[0]+dp[k+1];
+        
+             int i;
+             for(i=k;i<days.length&&days[i]<days[k]+7;i++);
+             int option2=costs[1]+dp[i];
+             
+             for(i=k;i<days.length&&days[i]<days[k]+30;i++);
+             int option3=costs[2]+dp[i];
+             
+             dp[k]=Math.min(option1,Math.min(option2,option3));
+         }
+         return dp[0];
+    }
     public int mincostTickets(int[] days, int[] costs) {
         int[]dp=new int[days.length];
         Arrays.fill(dp,-1);
-        return solveMem(days,costs,0,dp);
+        // return solveMem(days,costs,0,dp);
+        return solveTab(days,costs);
     }
 }
