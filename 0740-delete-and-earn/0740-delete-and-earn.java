79fd14c83b2f24224392e2070397aa5f41296540
Aviraj-create <81038098+Aviraj-create@users.noreply.github.com>
2023-04-05 11:36:27 +0530

    Time: 4 ms (72.82%), Space: 42.2 MB (85.42%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,51 +1,17 @@
-
-// class Solution {
-    
-//     public int f(int index,ArrayList<Integer>arr,HashMap<Integer,Integer>map)
-//     {
-//         if(arr.size()==0)return 0;
-        
-        
-        
-//     }
-//     public int deleteAndEarn(int[] nums) {
-//         HashMap<Integer,Integer>map=new HashMap<>();
-//         ArrayList<Integer>arr=new ArrayList<>();
-//         for(int num:nums)
-//         {
-//             arr.add(num);
-            
-//             if(!map.containsKey(num))
-//             {
-//                 map.put(num,1);
-//             }
-//             else
-//             {
-//                 map.put(num,map.get(num)+1);
-//             }
-//         }
-        
-//         int points=0;
-        
-//     }
-// }
 class Solution {
-  public int deleteAndEarn(int[] nums) {
-    // Reduce to 198. House Robber
-    int[] bucket = new int[10001];
-
-    for (final int num : nums)
-      bucket[num] += num;
-
-    int prev1 = 0;
-    int prev2 = 0;
-
-    for (final int num : bucket) {
-      final int dp = Math.max(prev1, prev2 + num);
-      prev2 = prev1;
-      prev1 = dp;
+    public int deleteAndEarn(int[] nums) {
+        
+        int []sum=new int[10001];
+        for(int num:nums){
+            sum[num]+=num;
+        }
+        int dp[]=new int[10001];
+        dp[1]=sum[1];
+        for(int i=2;i<10001;i++)
+        {
+            dp[i]=Math.max(dp[i-2]+sum[i],dp[i-1]);
+        }
+        return dp[10000];
+        
     }
-
-    return prev1;
-  }
 }
