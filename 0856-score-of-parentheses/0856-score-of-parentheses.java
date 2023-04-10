88df9354140e69a123febdb8f28eb7d63b018406
Aviraj-create <81038098+Aviraj-create@users.noreply.github.com>
2023-04-10 16:11:15 +0530

    Time: 1 ms (70.22%), Space: 40.2 MB (69.78%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.java
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -1,21 +1,50 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-       int curr=0;
+//        int curr=0;
+//         Stack<Integer>st=new Stack();
+//         for(char c:s.toCharArray())
+//         {
+//             if(c=='(')
+//             {
+//                 st.push(curr);
+//                 curr=0;
+//             }
+//             else
+//             {
+//                 curr=st.pop()+Math.max(curr*2,1);
+//             }
+//         }
+        
+//         return curr;
+        
         Stack<Integer>st=new Stack();
         for(char c:s.toCharArray())
         {
             if(c=='(')
             {
-                st.push(curr);
-                curr=0;
+                st.push(-1);
             }
             else
             {
-                curr=st.pop()+Math.max(curr*2,1);
+                int curr=0;
+                
+                while(st.peek()!=-1)
+                      {
+                          curr+=st.pop();
+                      }
+                      st.pop();
+                      st.push((curr==0)?1:curr*2);
+                    
             }
         }
         
-        return curr;
+        int sum=0;
+        while(!st.empty())
+        {
+            sum+=st.pop();
+        }
+        
+        return sum;
         
     }
 }
