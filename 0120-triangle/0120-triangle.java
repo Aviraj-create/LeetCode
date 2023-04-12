3d7384e9ba8aa608411858b0e57fda12fc8a7df9
Aviraj-create <81038098+Aviraj-create@users.noreply.github.com>
2023-04-12 23:25:33 +0530

    Time: 7 ms (26.64%), Space: 42.6 MB (26.28%) - LeetHub

---
+++ 0120-triangle/0120-triangle.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        
+        int row=triangle.size();
+        
+        for(int i=row-2;i>=0;i--)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                triangle.get(i).set(j,triangle.get(i).get(j)+Math.min(triangle.get(i+1).get(j),triangle.get(i+1).get(j+1)));
+                
+            }
+        }
+        return triangle.get(0).get(0);
+        
+        
+    }
+}
