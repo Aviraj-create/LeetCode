ba16b32b57b9fff5daf8e32f32ee3061bf0e0821
Aviraj-create <81038098+Aviraj-create@users.noreply.github.com>
2023-04-22 12:23:29 +0530

    Time: 22 ms (92.39%), Space: 44.1 MB (35.16%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,35 +1,19 @@
 class Solution {
-    int[][]dp;
-    public int f(int i,int j,String s,String s2,int[][]dp)
-    {
-        if(i<0 || j<0)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s.charAt(i)==s2.charAt(j))
-        {
-            return dp[i][j]=1+f(i-1,j-1,s,s2,dp);
-        }
-        else
-        {
-            return dp[i][j]=Math.max(f(i,j-1,s,s2,dp),f(i-1,j,s,s2,dp));
-        }
-    }
-    public int helper(String s)
-    {
-        StringBuilder sb=new StringBuilder();
-        sb.append(s);
-        sb.reverse();
-        String s2=sb.toString();
-        
-        return f(s.length()-1,s.length()-1,s,s2,dp);
-    }
     public int minInsertions(String s) {
-        dp=new int[s.length()][s.length()];
-        for(int i=0;i<s.length();i++)
+        int n=s.length();
+        int dp[][]=new int[n][n];
+        for(int i=n-1;i>=0;i--)
         {
-            Arrays.fill(dp[i],-1);
+            for(int j=i+1;j<n;j++)
+            {
+                if(s.charAt(i)==s.charAt(j))
+                    dp[i][j]=dp[i+1][j-1];
+                else
+                {
+                    dp[i][j]=Math.min(dp[i+1][j],dp[i][j-1])+1;
+                }
+            }
         }
-        
-        return s.length()-helper(s);
-        
+        return dp[0][n-1];
     }
 }
