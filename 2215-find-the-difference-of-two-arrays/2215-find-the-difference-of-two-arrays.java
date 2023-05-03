a5c7097159522a193f4711181817e8a674c48634
Aviraj-create <81038098+Aviraj-create@users.noreply.github.com>
2023-05-03 23:29:41 +0530

    Time: 11 ms (86.46%), Space: 43.5 MB (29.55%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -1,44 +1,34 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        
+        List<List<Integer>> answer=new ArrayList<>();
+        List<Integer> ans1=new ArrayList<>();
+        List<Integer> ans2=new ArrayList<>();
         Set<Integer> set1=new HashSet<>();
         Set<Integer> set2=new HashSet<>();
-
-        for(int i=0;i<nums1.length;i++)
-        {
-            set1.add(nums1[i]);
-        }
-        for(int i=0;i<nums2.length;i++)
-        {
-            set2.add(nums2[i]);
-        }
         
-        List<List<Integer>>answer=new ArrayList<>();
-        answer.add(new ArrayList<>());
-        answer.add(new ArrayList<>());
+        for(int i:nums1)
+            set1.add(i);
+        for(int i:nums2)
+            set2.add(i);
         
-        for(int i=0;i<nums1.length;i++)
+        for(int n:set1)
         {
-            if(set2.contains(nums1[i]))
+            if(!set2.contains(n))
             {
-                continue;
+                ans1.add(n);
             }
-            if(!answer.get(0).contains(nums1[i]))
-            {
-                answer.get(0).add(nums1[i]);
-            }
-            
         }
-        
-        for(int i=0;i<nums2.length;i++)
+        for(int n:set2)
         {
-            if(set1.contains(nums2[i]))
+            if(!set1.contains(n))
             {
-                continue;
+                ans2.add(n);
             }
-            if(!answer.get(1).contains(nums2[i]))
-            {answer.get(1).add(nums2[i]);}
         }
         
+        answer.add(ans1);
+        answer.add(ans2);
         return answer;
     }
 }
