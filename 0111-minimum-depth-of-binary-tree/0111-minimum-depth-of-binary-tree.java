ecd2f3ad6bc1ea577bc81bc284c775f2087d9c38
Aviraj-create <81038098+Aviraj-create@users.noreply.github.com>
2023-07-10 12:08:31 +0530

    Time: 1 ms (99.31%), Space: 61.1 MB (92.02%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -13,12 +13,39 @@
  *     }
  * }
  */
-class Solution {
-    public int minDepth(TreeNode root) {
-        if(root==null)return 0;
-        int lh=minDepth(root.left);
-        int rh=minDepth(root.right);
+// class Solution {
+//     public int minDepth(TreeNode root) {
+//         if(root==null)return 0;
+//         int lh=minDepth(root.left);
+//         int rh=minDepth(root.right);
         
-        return (lh==0 || rh==0)?lh+rh+1:1+Math.min(lh,rh);
+//         return (lh==0 || rh==0)?lh+rh+1:1+Math.min(lh,rh);
+//     }
+// }
+
+class Solution {
+    public int minDepth(TreeNode root)
+    {
+        Queue<TreeNode> q=new LinkedList<>();
+        if(root==null)return 0;
+        q.add(root);
+        int depth=0;
+        
+        while(!q.isEmpty())
+        {
+            depth++;
+            int n=q.size();
+            for(int i=0;i<n;i++)
+            {
+               TreeNode temp= q.poll();
+               if(temp.left==null && temp.right==null)return depth;
+                if(temp.left!=null)q.add(temp.left);
+                if(temp.right!=null)q.add(temp.right);
+            }
+            
+        }
+        
+        return depth;
+        
     }
 }
