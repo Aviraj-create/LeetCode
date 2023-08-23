a49a04646af4a1832a86396a6f6fe27220dd5682
Aviraj Singh Chauhan <81038098+Aviraj-create@users.noreply.github.com>
2023-08-23 12:27:28 +0530

    Time: 3 ms (68.33%), Space: 40.2 MB (95.95%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -0,0 +1,48 @@
+class Solution {
+    public class pair{
+        char ch;
+        int f;
+        
+        public pair(char ch,int f)
+        {
+            this.ch=ch;
+            this.f=f;
+        }
+    }
+    public String reorganizeString(String s) {
+        
+        int[] map=new int[26];
+        for(int i=0;i<s.length();i++)
+        {
+            map[s.charAt(i)-'a']++;
+        }
+        
+        PriorityQueue<pair> pq=new PriorityQueue<>((a,b)->b.f-a.f);
+        for(int i=0;i<26;i++)
+        {
+            if(map[i]>0)
+            {
+                pq.add(new pair((char)(i+'a'),map[i]));
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        
+        pair block=pq.poll();
+        sb.append(block.ch);
+        block.f--;
+        while(pq.size()>0)
+        {
+            pair temp=pq.poll();
+            sb.append(temp.ch);
+            temp.f--;
+            if(block.f>0)
+            {
+                pq.add(block);
+            }
+            block=temp;
+        }
+        if(block.f>0)return "";
+        return sb.toString();
+        
+    }
+}
