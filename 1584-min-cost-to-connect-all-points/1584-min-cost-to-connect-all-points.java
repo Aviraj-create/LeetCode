22532167187c2b138226f1b6d0c7a142cbee4007
Aviraj Singh Chauhan <81038098+Aviraj-create@users.noreply.github.com>
2023-09-15 10:11:36 +0530

    Time: 70 ms (88.81%), Space: 59.3 MB (64.52%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,21 +1,22 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        
         int n=points.length;
-        int ans=0;
-        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
-        for(int i=0;i<n;i++)
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        for(int i=0;i<n-1;i++)
         {
             for(int j=i+1;j<n;j++)
             {
-                pq.add(new int[]{findDis(points,i,j),i,j});
+                pq.offer(new int[]{dis(i,j,points),i,j});
             }
+            
         }
+        
+        UnionFind uf=new UnionFind(n);
+        int ans=0;
         int count=0;
-         UnionFind uf=new UnionFind(n);
         while(count<n-1)
         {
-           int[] temp=pq.poll();
+            int[] temp=pq.poll();
             if(uf.find(temp[1])!=uf.find(temp[2]))
             {
                 ans+=temp[0];
@@ -24,20 +25,22 @@ class Solution {
             }
         }
         return ans;
+        
+            
     }
     
-    private int findDis(int [][]points,int a,int b)
+    public int dis(int i,int j, int[][] points)
     {
-        return Math.abs(points[a][0]-points[b][0])+Math.abs(points[a][1]-points[b][1]);
+        return Math.abs(Math.abs(points[i][0]-points[j][0])+Math.abs(points[i][1]-points[j][1]));
     }
     
     class UnionFind
     {
-        int[]parent;
+         int[] parent;
         UnionFind(int n)
         {
             this.parent=new int[n];
-            for(int i=0;i<n;i++)
+            for(int i=0;i<parent.length;i++)
             {
                 parent[i]=i;
             }
@@ -47,12 +50,13 @@ class Solution {
         {
             parent[find(a)]=parent[find(b)];
         }
-        
         public int find(int x)
         {
-            if(parent[x] != x) parent[x] = find(parent[x]);
+            if(parent[x]!=x)
+            {
+              parent[x]=find(parent[x]);
+            }
             return parent[x];
-        }
-        
+        } 
     }
 }
