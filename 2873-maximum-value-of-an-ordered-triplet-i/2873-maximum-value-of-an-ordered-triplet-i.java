ab43f63070f7576417bb3e8a497dea1973da29c1
Aviraj Singh Chauhan <81038098+Aviraj-create@users.noreply.github.com>
2023-10-01 19:32:32 +0530

    Time: 1 ms (100.00%), Space: 41.1 MB (25.00%) - LeetHub

--- 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.java
+++ 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.java
@@ -1,18 +1,34 @@
 class Solution {
     public long maximumTripletValue(int[] nums) {
-        long ans=0;
-        for(int i=0;i<nums.length-2;i++)
-        {
-            for(int j=i+1;j<nums.length-1;j++)
-            {
-                for(int k=j+1;k<nums.length;k++)
-                {
+//         long ans=0;
+//         for(int i=0;i<nums.length-2;i++)
+//         {
+//             for(int j=i+1;j<nums.length-1;j++)
+//             {
+//                 for(int k=j+1;k<nums.length;k++)
+//                 {
                     
-                    long currScore = (long)(nums[i] - nums[j]) * nums[k];
-                    ans = Math.max(ans, currScore);
-                }
-            }
+//                     long currScore = (long)(nums[i] - nums[j]) * nums[k];
+//                     ans = Math.max(ans, currScore);
+//                 }
+//             }
+//         }
+//         return ans;
+        
+        long score=0;
+        int n=nums.length;
+        int[] maxLeft=new int[n];
+        int[] maxRight=new int[n];
+        maxLeft[0]=nums[0];
+        maxRight[n-1]=nums[n-1];
+        for(int i=1;i<n;i++) maxLeft[i]=Math.max(maxLeft[i-1],nums[i]);
+        for(int i=n-2;i>=0;i--) maxRight[i]=Math.max(maxRight[i+1],nums[i]);
+        for(int i=1;i<n-1;i++)
+        {
+            long currScore = (long)(maxLeft[i-1]-nums[i])*maxRight[i+1];
+            score = Math.max(score, currScore);
         }
-        return ans;
+        return score;
+        
     }
 }
